2019a7353e4074f89ccfbdc3413bce0e6e56b6d7
mjt_milestone <mjt@milestone.dk>
2025-04-17 19:55:56 -0500

    B: added constants.py .gitignore and main function for main.py

--- main.py
+++ main.py
@@ -2,9 +2,21 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    # Initialize the game
+    pygame.init()
+    print("Starting Asteroids")    
+    # Set up the game window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
