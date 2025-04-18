3f7343e3bccf5bf6233fcfe7c2a4d7eb77510005
mjt_milestone <mjt@milestone.dk>
2025-04-18 11:53:44 -0500

    D: added circleshape.py and player.py instantiated player sprite in main function game loop

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     # Initialize the game
@@ -13,12 +14,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
 
+    # Create the player object in the center of the screen
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y, PLAYER_RADIUS, 0)
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False  # Exit the game loop
 
+        # Handle game logic and rendering here
+        # Fill the screen with black
         screen.fill("black")  # Clear the screen with black
+        player.draw(screen)
         pygame.display.flip()  # Update the display
 
         dt = clock.tick(60) / 1000  # Limit to 60 FPS and get delta time in seconds
